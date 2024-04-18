64d8a1368d33115cf430c95bdf070641ebb60940
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-04-18 18:13:50 +0700

    feat: Transfer

--- main.c
+++ main.c
@@ -74,6 +74,39 @@ int chooseMenu(char menus[10][20])
     }
 }
 
+bool tryAskForPin(User* user)
+{
+    int failPinTimes = 0;
+
+    while (1)
+    {
+        int pin;
+
+        printf("Pin:");
+        scanf("%d", &pin);
+
+        if (user->pin != pin)
+        {
+            printf("Wrong pin.\n");
+
+            failPinTimes += 1;
+
+            if (failPinTimes >= 3)
+            {
+                printf("Pin wrong 3 times. User deactivated.\n");
+
+                user->active = false;
+
+                return false;
+            }
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
+
 int main(void)
 {
     createUser("Foo", 123123, 10000);
@@ -152,61 +185,108 @@ int main(void)
 
                 while (1)
                 {
-                    char loggedInMenus[10][20] = {"Check Balance", "Logout", "Change Pin"};
+                    char loggedInMenus[10][20] = {"Check Balance", "Logout", "Change Pin", "Transfer"};
                     int loggedInMenuChoice = chooseMenu(loggedInMenus);
 
                     switch (loggedInMenuChoice)
                     {
                     case 0:
-                        {
-                            printf("Your balance is %f\n", user.balance);
+                        printf("Your balance is %f\n", user.balance);
 
-                            break;
-                        }
+                        break;
                     case 1:
+                        printf("Logged out.\n");
+
+                        goto logout;
+                    case 2:
                         {
-                            printf("Logged out.\n");
+                            printf("Change pin.\n");
+
+                            bool isPinCorrect = tryAskForPin(&user);
+
+                            if (!isPinCorrect)
+                                goto logout;
+
+                            int newPin;
+
+                            printf("New Pin:");
+                            scanf("%d", &newPin);
+
+                            if (user.pin == newPin)
+                            {
+                                printf("New pin cannot be same as current pin.\n");
+
+                                break;
+                            }
+
+                            user.pin = pin;
+
+                            printf("Pin changed.\n");
 
                             goto logout;
                         }
-                    }
-                case 2:
-                    {
+                    case 3:
+                        {
                             // Wrong here. Always go here.
-                        printf("Change pin.\n");
+                            printf("Transfer.\n");
 
-                        int currentPin, newPin;
+                            bool isPinCorrect = tryAskForPin(&user);
 
-                        printf("Current Pin:");
-                        scanf("%d", &currentPin);
-                        printf("New Pin:");
-                        scanf("%d", &newPin);
+                            if (!isPinCorrect)
+                                goto logout;
 
-                        if (user.pin != currentPin)
-                        {
-                            printf("Wrong current pin.\n");
+
+                            int transferAmount;
+
+                            printf("Transfer Amount:");
+                            scanf("%d", &transferAmount);
+
+                            if (transferAmount > user.balance)
+                            {
+                                printf("Not enough balance\n");
+
+                                break;
+                            }
+
+                            int targetUserID;
+
+                            printf("Target User ID:");
+                            scanf("%d", &targetUserID);
+
+                            int foundUserIndex = -1;
+
+                            for (int i = 0; i < userIndexCounter + 1; ++i)
+                            {
+                                User user = users[i];
+
+                                if (targetUserID == user.id)
+                                {
+                                    foundUserIndex = i;
+                                }
+                            }
+
+                            if (foundUserIndex == -1)
+                            {
+                                printf("Target user with id %d not found\n", targetUserID);
+
+                                break;
+                            }
+
+                            User targetUser = users[foundUserIndex];
+
+                            user.balance -= transferAmount;
+                            targetUser.balance += transferAmount;
+
+                            printf("Succesfully transfered %d to user with the id %d.\n", transferAmount, targetUserID);
 
                             break;
                         }
-
-                        if (user.pin != newPin)
-                        {
-                            printf("New pin cannot be same as current pin.\n");
-
-                            break;
-                        }
-
-                        user.pin = pin;
-
-                        printf("Pin changed.\n");
-
-                        goto logout;
                     }
                 }
             }
-        }
 
-    logout:
-        void;
+        logout:
+            void;
+        }
     }
 }
